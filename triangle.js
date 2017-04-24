b6b798a503621e201ba0befd866e094ce6f4b9c7
alu0100785265 <alu0100785265@ull.edu.es>
2017-04-24 09:18:41 +0000

    cambios

---
+++ triangle.js
@@ -0,0 +1,13 @@
+var nuevo = require("@alu0100785265/ull-shape-lucas");
+var Shape = nuevo.Shape;
+
+class Triangle extends Shape {
+    constructor(options) {
+      super(options)
+    }
+    area() {
+      return 0.5 * this.width * this.height;
+    }
+}
+Shape.Shapes.Triangle = Triangle;
+module.exports = Triangle;
