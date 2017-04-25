0c0de3aaa7a8d787651090e2f81271373f6f9f29
alu0100785265 <alu0100785265@ull.edu.es>
2017-04-25 09:34:42 +0000

    pruebas

--- test/triangulo.js
+++ test/triangulo.js
@@ -1,15 +1,13 @@
 "use strict"
 
-var Tri = require('../triangle.js')
-var Shape = require("@alu0100785265/ull-shape-lucas")
-var Triangle = Tri.Triangle
-var should = require('should');
 
+var should = require('should');
+let Triangle = require("../lib/triangle");
 
 
 describe("getArea", function() {
   it("must compute the triangle area correctly. Shape object", function() {
-    let a = new Shape({ width: 100, height: 50 }, 'Triangle');
+    let a = new Triangle({ width: 100, height: 100 });
     let s = a.getArea();
     s.should.match(/^5000$/);
   })
